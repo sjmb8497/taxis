f275a55a1c5a2a2e83f477373e2a674d8c7cd384
Sophia <montesbolahs@elsevier.com>
2021-07-05 22:58:04 +0100

    Display taxis and find closest office to user

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,78 @@
 import React, { useState, useEffect } from 'react';
-import { Map } from './Map';
+import { Map, driverDataInterface } from './Map';
 import { getTaxis } from "./api";
 
 const App: React.FunctionComponent = () => {
-  const [driverData, setDriverData] = useState([]);
+  const [driverData, setDriverData] = useState<driverDataInterface>({
+    pickup_eta: 1,
+    drivers: [],
+  });
+  interface locationInterface {
+    lat: number;
+    lng: number;
+}
+  const [userLocation, setUserLocation] = useState<locationInterface>({
+    lat: 1.285194,
+    lng: 103.8522982
+  });
+
+  const [center, setCenter] = useState<locationInterface>({
+    lat: 1.285194,
+    lng: 103.8522982
+  });
+
+
+  const distance = (lat1: number, lon1: number, lat2: number, lon2: number) => {
+    if ((lat1 == lat2) && (lon1 == lon2)) {
+      return 0;
+    }
+    else {
+      var radlat1 = Math.PI * lat1/180;
+      var radlat2 = Math.PI * lat2/180;
+      var theta = lon1-lon2;
+      var radtheta = Math.PI * theta/180;
+      var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+      if (dist > 1) {
+        dist = 1;
+      }
+      dist = Math.acos(dist);
+      dist = dist * 180/Math.PI;
+      dist = dist * 60 * 1.1515;
+      return dist;
+    }
+  }
+
+  const findClosestOffice = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setUserLocation({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        });
+        console.log('userLocation', userLocation)
+      })
+    }
+    const singaporeDistance = distance(userLocation.lat, userLocation.lng, 1.285194, 103.8522982);
+    const londonDistance = distance(userLocation.lat, userLocation.lng, 51.5049375, -0.0964509);
+
+    if (londonDistance < singaporeDistance) {
+      setCenter({lat: 51.5049375, lng:-0.0964509})
+    }
+  }
+
+  //find user location (long and lat)
+  //find distance to singapore
+  //find distance to london
+  // if singapore distance < london distance, center = singapore long and lat
+  // else center = london long and lat
+
   useEffect(() => {
     async function fetch() {
       try {
         const data = await getTaxis();
-        console.log('sophia')
         console.log('data', data)
         setDriverData(data);
-        // console.log(driverData)
+        findClosestOffice();
       } catch (err) {
         console.error(err);
       }
@@ -19,15 +80,9 @@ const App: React.FunctionComponent = () => {
     fetch()
   }, [])
 
-  const defaultCenter = {
-    lat: 1.285194,
-    lng: 103.8522982
-  }
-
   return (
     <div className="App">
-      <p>hello world</p>
-      <Map taxis={driverData} center={defaultCenter}/>
+      <Map taxis={driverData} center={center}/>
     </div>
   );
 }
--- client/src/Map.tsx
+++ client/src/Map.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import GoogleMapReact from 'google-map-react';
+import styled from 'styled-components';
+import GoogleMapReact, { Coords } from 'google-map-react';
 
 type Props = {
-    taxis: any;
+    taxis: driverDataInterface;
     center: center;
 };
 
@@ -10,26 +11,44 @@ interface center {
     lat: number;
     lng: number;
 }
+interface driver {
+    driver_id: string;
+    location: {
+      latitude: number;
+      longitude: number;
+      bearing: number;
+    };
+  }
+  
+export interface driverDataInterface {
+    pickup_eta: number;
+    drivers: driver[];
+}
 
-const Marker = ({text}: any) => <div>{text}</div>;
+const Marker = styled.div<Coords>`
+  height: 15px;
+  width: 15px;
+  border-radius: 50%;
+  background: #3700ff;
+`;
 
-export const Map: React.FunctionComponent<Props> = ({ taxis }) => {
-    const [center, setCenter] = useState({lat: 1.285194, lng: 103.8522982 });
-    const [zoom, setZoom] = useState(11);
+export const Map: React.FunctionComponent<Props> = ({ taxis, center }) => {
+    const [zoom, setZoom] = useState(13);
     return (
-        <div style={{ height: '90vh', width: '90vw' }}>
-        <GoogleMapReact
-        bootstrapURLKeys={{ key: "AIzaSyDha5iMRcmLfdQ-c4t7eu5dEoZXYGmVri4" }}
-        defaultCenter={center}
-        defaultZoom={zoom}
-        >
-        <Marker
-            lat={1.285194}
-            lng={103.8522982}
-            text="My Marker"
-          />
-
-        </GoogleMapReact>
+        <div style={{ height: '100vh', width: '100vw' }}>
+            <GoogleMapReact
+            bootstrapURLKeys={{ key: "AIzaSyDha5iMRcmLfdQ-c4t7eu5dEoZXYGmVri4" }}
+            center={center}
+            defaultZoom={zoom}
+            >
+            {taxis.drivers.length && 
+            taxis.drivers.map((taxi) => (
+                <Marker
+                    lat={taxi.location.latitude}
+                    lng={taxi.location.longitude}
+                />
+            ))}
+            </GoogleMapReact>
         </div>
     )
 }
